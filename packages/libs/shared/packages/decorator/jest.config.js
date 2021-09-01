29b81c53568ee089d5ec3afa3ddf3ade17c076fa
yuyiRai <yuyi919606952@foxmail.com>
2021-09-01 17:49:10 +0800

    init

--- packages/libs/shared/packages/decorator/jest.config.js
+++ packages/libs/shared/packages/decorator/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   displayName: "shared-decorator",
   preset: "./node_modules/@yuyi919/workspace-base-rig/jest.preset.js",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../../coverage/libs/shared/decorator",
+  coverageDirectory: "../../../coverage/libs/shared/packages/decorator",
 };
