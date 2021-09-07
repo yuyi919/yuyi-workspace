a8c628cee2f7206111d785b03b86499647cf8133
yuyiRai <yuyi919606952@foxmail.com>
2021-09-07 20:22:11 +0800

    init

--- packages/libs/shared/packages/utils/jest.config.js
+++ packages/libs/shared/packages/utils/jest.config.js
@@ -1,15 +1,6 @@
 module.exports = {
-  displayName: "shared-packages-utils",
+  displayName: "shared-utils",
   preset: "./node_modules/@yuyi919/workspace-base-rig/jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/tsconfig.spec.json",
-      module: "esnext",
-    },
-  },
-  transform: {
-    "^.+\\.[tj]sx?$": "ts-jest",
-  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../../../coverage/packages/shared/packages/utils",
+  coverageDirectory: "../../../../coverage/libs/shared/packages/utils",
 };
