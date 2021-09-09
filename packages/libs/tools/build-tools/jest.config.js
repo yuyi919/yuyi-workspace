ba62748919a6c72c86382f26efe1f44b06eb3417
yuyiRai <yuyi919606952@foxmail.com>
2021-09-09 17:19:16 +0800

    init

--- packages/libs/tools/build-tools/jest.config.js
+++ packages/libs/tools/build-tools/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: "tools-build-tools",
+  displayName: "internal-build-tools",
   preset: "./node_modules/@yuyi919/workspace-base-rig/jest.preset.js",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "./node_modules/@yuyi919/workspace-base-rig/coverage/libs/tools/build-tools",
